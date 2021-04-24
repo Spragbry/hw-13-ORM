430bd52af69d77d94c0e1415a496dcd3962454c6
Bryce Spraggins <spragbry93@gmail.com>
2021-04-23 22:40:37 -0400

    mysql

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,33 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE category (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    catergory_name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE product (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES catergory(id) on DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL AUTO_INCREMENT,
+    tag_name VARCHAR (30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ProductTag (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(id) on DELETE CASCADE,
+    tag_id INT,
+    FOREIGN KEY (tag_id) REFERENCES tag(id) on DELETE CASCADE,
+    PRIMARY KEY (id)
+);
--- server.js
+++ server.js
@@ -1,6 +1,74 @@
 const express = require('express');
 const routes = require('./routes');
+const { DataTypes } = require('sequelize');
 // import sequelize connection
+import sequelize from "./config/connection.js";
+const Category= sequelize.define('Category', {
+  // Model attributes are defined here
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  category_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {});
+const Product= sequelize.define('Product', {
+  // Model attributes are defined here
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  stock: {
+    type: DataTypes.STRING,
+    allowNull: false 
+  },
+    category_id: {
+      type: DataTypes.STRING,
+      allowNull: false }
+},{});
+
+const Tag = sequelize.define('Tag', {
+  // Model attributes are defined here
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  tag_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {});
+const ProductTag = sequelize.define('Product', {
+  // Model attributes are defined here
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  product_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  tag_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+},{});
+
+Category.hasMany(Product);
+Product.hasOne(Category);
+Product.hasMany(Tag);
+Tag.hasMany(Product);
+Product.belongsToMany(Tag, { through: "ProductTag" });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
